2e722cba00de24a60fb719501158ad44b55073c4
denis <denis@peyrusaubes.com>
2018-08-28 15:17:09 +1100

    modif conf karma avec juint

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -12,19 +12,18 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-jenkins-reporter')
-
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly','junit'],
       fixWebpackSourcePaths: true
     },
     junitReporter    : {
-      outputDir : 'target/surefire-reports/'
+      outputFile: 'test-results.xml'
     },
     reporters: ['progress', 'junit', 'kjhtml'],
     port: 9876,
