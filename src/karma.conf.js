977cd7196a6e3cb728ef9fa9ab07567544a59d32
denis <denis@peyrusaubes.com>
2018-08-28 15:56:41 +1100

    remove CHROME_BIN env variable

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -4,8 +4,6 @@
 
 
 module.exports = function (config) {
-  const process = require('process');
-  process.env.CHROME_BIN = require('puppeteer').executablePath();
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
